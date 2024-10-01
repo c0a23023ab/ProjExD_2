341e51baf4bcc3ee8a5f8b27a658d8e1205bca1b
C0A23023 <c0a23023ab@edu.teu.ac.jp>
2024-10-01 15:15:48 +0900

    練習4:ゲーム終了

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -70,6 +70,8 @@ def main():
         if not yoko:
             vx *= -1
         screen.blit(kk_img, kk_rct)
+        if kk_rct.colliderect(bb_rct):
+            return 
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
